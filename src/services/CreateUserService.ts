ce3d35b3ea4fa6f5424abad0c00d033501578843
Ramon Papes <ramonpapes@gmail.com>
2024-05-25 15:18:55 -0300

    fix: add user password in service

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,13 +1,15 @@
 import { UserRepository } from "../repositories/UserRepository";
+import { hash } from "bcryptjs";
 
 interface IUserRequest {
     name: string;
     email: string;
+    password: string;
     admin?: boolean;
 }
 
 class CreateUserService {
-    async execute({ name, email, admin }: IUserRequest) {
+    async execute({ name, email, password, admin }: IUserRequest) {
         if (!email) {
             throw new Error("Email incorrect");
         }
@@ -17,15 +19,28 @@ class CreateUserService {
             throw new Error("User already exists");
         }
 
+        const passwordHash = await hash(password, 8);
+
         const user = UserRepository.create({
             name,
             email,
             admin,
+            password: passwordHash,
         })
 
         await UserRepository.save(user);
 
-        return user;
+        const userWhithoutMetadata = {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            admin: user.admin,
+            created_at: user.created_at,
+            updated_at: user.updated_at,
+        }
+
+
+        return userWhithoutMetadata;
     }
 }
 
